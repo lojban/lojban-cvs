6a76e1d60b5bc01adc2a0d127616b4fa187f76fe
Robin Lee Powell <rlpowell@jukni.digitalkingdom.org>
2020-01-20 11:17:30 -0800

    Cleanup: removing useless duplication

--- cvs/festvox/kal_diphone_jbo_us1/vocabsay/dump-jbo2en.scm
+++
@@ -1,8 +0,0 @@
-
-;; dump a terse vocabulary, in scheme form, to stdout
-;; this may be used, for example, for download to a PDA
-
-(load "jbo-vocab.scm")
-(dump-jbo2en "vocab-words.scm") ; <- input filename, output is to stdout
-
-
